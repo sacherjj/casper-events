b4639cfe949e65264c513c1f157bddbd49995c32
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-03-30 15:23:48 -0400

    Stubbed out finality generation.

--- file_store.py
+++ file_store.py
@@ -1,10 +1,13 @@
+from typing import Union
 from event_stream_reader import EventStreamReader
-from config import DATA_DIR, SSE_SERVER_URL
+import config
 from message_structure import MessageData
 from pathlib import Path
+from generate_finality_signatures import generate_finality_signatures_for_block
+import json
 
 
-esr = EventStreamReader(SSE_SERVER_URL)
+esr = EventStreamReader(config.SSE_SERVER_URL)
 
 # Three message types:
 # DeployProcessed - Has no era_id, can store in block-hash folder and move into era_id folder when we get BlockAdded
@@ -15,11 +18,11 @@ esr = EventStreamReader(SSE_SERVER_URL)
 #   Final location: era_<era_id>/<block_hash>/finsig-<block_hash>-<public_key>
 
 
-def era_directory_name(era_id: str) -> str:
+def era_directory_name(era_id: Union[str,int]) -> str:
     return f"era_{era_id}"
 
 
-def save_file_in_directory(directory: str, filename: str, contents: str, root_dir: Path = DATA_DIR):
+def save_file_in_directory(directory: str, filename: str, contents: str, root_dir: Path = config.DATA_DIR):
     """ Creates directory if needed and saves file to filename in directory """
     target_dir = root_dir / directory
     target_dir.mkdir(parents=True, exist_ok=True)
@@ -27,7 +30,7 @@ def save_file_in_directory(directory: str, filename: str, contents: str, root_di
     file_path.write_text(contents)
 
 
-def move_deploys_to_era(directory: str, era_id: str, root_dir: Path = DATA_DIR):
+def move_deploys_to_era(directory: str, era_id: str, root_dir: Path = config.DATA_DIR):
     """ Moves all temp stored deploys into the proper era directory """
     source_dir = root_dir / directory
     target_dir = root_dir / era_directory_name(era_id) / directory
@@ -38,21 +41,55 @@ def move_deploys_to_era(directory: str, era_id: str, root_dir: Path = DATA_DIR):
         source_dir.rmdir()
 
 
-for msg in esr.messages():
-    if not msg:
-        continue
-    data = MessageData(msg.data)
-    era_id = data.era_id
-    # We don't have a block for deploys yet as they process before the era_id is known.
-    # Using a directory name in root data directory as block_hash
-    directory = era_directory_name(era_id) if not data.is_deploy_processed else data.block_hash
+def save_files():
+    for msg in esr.messages():
+        if not msg:
+            continue
+        data = MessageData(msg.data)
+        era_id = data.era_id
+        # We don't have a block for deploys yet as they process before the era_id is known.
+        # Using a directory name in root data directory as block_hash
+        directory = era_directory_name(era_id) if not data.is_deploy_processed else data.block_hash
 
-    # We can go directly into a era/block_hash structure for finality_signatures
-    if data.is_finality_signature:
-        directory += f"/{data.block_hash}"
-    # Deploys are made into block-<block_hash> directory that needs to be moved once BlockAdded test is what era the
-    # Block was in.
-    save_file_in_directory(directory, data.primary_key, msg.data)
-    if data.is_block_added:
-        # WHen a block is added, we know what the block era is for deploys stored, so we can copy them over.
-        move_deploys_to_era(data.block_hash, era_id)
+        # We can go directly into a era/block_hash structure for finality_signatures
+        if data.is_finality_signature:
+            directory += f"/{data.block_hash}"
+        # Deploys are made into block-<block_hash> directory that needs to be moved once BlockAdded test is what era the
+        # Block was in.
+        save_file_in_directory(directory, data.primary_key, msg.data)
+        if data.is_block_added:
+            # WHen a block is added, we know what the block era is for deploys stored, so we can copy them over.
+            move_deploys_to_era(data.block_hash, era_id)
+
+
+def get_era_directories(data_dir: Path = config.DATA_DIR):
+    """ return era directory Paths in order of era """
+    return sorted([era_dir for era_dir in data_dir.glob("era_*")], key=lambda d: int(str(d).split('era_')[-1]))
+
+
+def get_block_hashes_from_dir(era_dir: Path):
+    for block_file in era_dir.glob("block-*"):
+        yield block_file.name.split("block-")[-1]
+
+
+def is_missing_finsig_files(hash_dir: Path) -> bool:
+    if not hash_dir.exists():
+        return True
+    return len(list(hash_dir.glob("finsig*"))) == 0
+
+
+def recreate_finality_signatures(data_dir: Path = config.DATA_DIR):
+    for era_dir in get_era_directories(data_dir):
+        for hash in get_block_hashes_from_dir(era_dir):
+            finsig_path = era_dir / hash
+            if is_missing_finsig_files(finsig_path):
+                try:
+                    for finsig in generate_finality_signatures_for_block(hash):
+                        data = MessageData(json.dumps(finsig))
+                        save_file_in_directory(f"{era_dir.name}/{hash}", data.primary_key, finsig)
+                        print(finsig)
+                except Exception as e:
+                    print(e)
+
+
+# recreate_finality_signatures()
